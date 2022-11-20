65763f95c5a9681c7c7e09ba30e97fde2f903a32
Nazar <nbuievskyi@gmail.com>
2022-11-20 19:14:41 +0100

    ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: 'class',
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'twitter' : '#1DA1F2',
+        'linkedIn' : '#0e76a8',
+        'youtube' : '#FF0000',
+      },
+      fontFamily: {
+        burtons: 'Burtons',
+      }
+    },
   },
   plugins: [],
 }
